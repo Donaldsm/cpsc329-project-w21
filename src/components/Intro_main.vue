8b04fc555a038f800ac845e542c763bac9550af7
LougheedWan <47652087+LougheedWan@users.noreply.github.com>
2021-03-25 13:26:34 -0600

    testcommit

---
+++ src/components/Intro_main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="intro">
+    <h1>Think You Are Safe On A Computer?</h1>
+
+    <h2>Lets Find out!</h2>
+
+    <h3>Click the Link below to start!</h3>
+
+    <button class = "bg-blue-500 hover:bg-blue-400 text-white font-bold">Go!</button>
+    <div id="terms">
+    <router-link to="/termsandcons">terms and conditions</router-link>
+    </div>
+  </div>
+</template>
+
+
+<style lang="less">
+#terms{
+    padding: 30px;
+}
+</style>
