f40aa0ce56852dcc0880bac56959fd8d635a67b0
Donaldsm <Donald@theMcEacherns.com>
2021-04-01 14:27:48 -0600

    finished UI for second question

--- src/views/Quiz1.vue
+++ src/views/Quiz1.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="flex flex-container">
         <Messenger class="mx-auto"></Messenger>
+        <identification class="mx-auto"></identification>
     </div>
 </template>
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
 import Messenger from '../components/Messenger.vue';
+import Identification from '../components/Identification.vue';
 
 @Component({
     components: {
         Messenger,
+        Identification,
     },
 })
 export default class Quiz1 extends Vue {
