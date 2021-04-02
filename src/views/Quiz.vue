0575979c4f087c9bd8dc6e73f7e81743b134e7ca
Michael Funk <michael.funk@ucalgary.ca>
2021-04-01 18:33:49 -0600

    made q5 card lol

---
+++ src/views/Quiz.vue
@@ -0,0 +1,32 @@
+
+<template>
+    <div>
+
+      <!-- <Q1Card/> -->
+      
+       <Q4Card/>
+    </div>
+</template>
+
+
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import Q1Card from '@/components/Question1Card.vue';
+import Q4Card from '@/components/Question4.vue';
+import Q5Card from '@/components/Question5.vue';
+
+
+@Component({
+  components: {
+    Q1Card,
+    Q5Card,
+    Q4Card,
+  }
+})
+export default class Quiz1 extends Vue{
+
+}
+</script>
+
+
