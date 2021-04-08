b1756424aae181d26861974e8d74c02c9277d32e
sarinazohdi <sarinazohdi123@gmail.com>
2021-04-07 22:15:09 -0600

    hehehehe

---
+++ src/components/Question3.vue
@@ -0,0 +1,28 @@
+<template> 
+<div>
+<div class ="flex justify-center flex-col px-10">
+
+<h1 class="text-3xl font-extrabold pb-6"> Look at the comic below and answer the following questions </h1>
+
+</div>
+ 
+<div class="row">
+
+<img src="img/Q3.png" alt=""/>
+
+<h3> Do you think they should share their private key online to get support? </h3>
+
+  <input type="radio" v-model="color" value="true">yes
+  <input type="radio" v-model="color" value="false">no
+</div> 
+</div>
+</template> 
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Q3Card extends Vue {
+  @Prop() private msg!: string;                                                                                                                                                                                                    
+}
+</script>
--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -4,6 +4,7 @@
 
       <!-- <Q1Card/> -->
        <Q5Card/>
+       <Q3Card/>
     </div>
 </template>
 
@@ -12,6 +13,7 @@
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
 import Q1Card from '@/components/Question1Card.vue';
+import Q3Card from '@/components/Question3.vue';
 import Q4Card from '@/components/Question4.vue';
 import Q5Card from '@/components/Question5.vue';
 
@@ -19,6 +21,7 @@ import Q5Card from '@/components/Question5.vue';
 @Component({
   components: {
     Q1Card,
+    Q3Card,
     Q5Card,
     Q4Card,
   }
