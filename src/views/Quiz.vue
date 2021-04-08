c8bdf40d116048150b9ee066bee8770be8de2341
Donaldsm <Donald@theMcEacherns.com>
2021-04-08 14:08:47 -0600

    added quiz navigation.

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -2,13 +2,26 @@
 <template>
   <div class="">
     <div class="flex flex wrap">
-      <button v-if="this.view != 0" @click="changePrevView()" class="m-3 bg-blue-300 w-1/4 mx-auto">Previous Question</button>
-      <button v-if="this.view != 9" @click="changeNextView()" class="m-3 bg-blue-300 w-1/4 mx-auto">Next Question</button>
+      <button
+        v-if="this.view != 0"
+        @click="changePrevView()"
+        class="max-w-xs bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mx-auto"
+      >
+        Prev Question
+      </button>
+      <button
+        v-if="this.view != 9"
+        @click="changeNextView()"
+        class="max-w-xs bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mx-auto"
+      >
+        Next Question
+      </button>
     </div>
 
     <div>
+      <!-- <TC/> -->
       <div v-if="this.view == 0">
-        <!-- <Q1Card/> -->
+        <Q1Card />
       </div>
       <div v-if="this.view == 1">
         <identification class="mx-auto m-3"></identification>
@@ -35,17 +48,16 @@
 
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
-import Q1Card from '@/components/Question1Card.vue';
-import Q3Card from '@/components/Question3.vue';
-import Q4Card from '@/components/Question4.vue';
-import Q5Card from '@/components/Question5.vue';
+import { Component, Vue } from "vue-property-decorator";
+import Q1Card from "@/components/Question1Card.vue";
+import Q3Card from "@/components/Question3.vue";
+import Q4Card from "@/components/Question4.vue";
+import Q5Card from "@/components/Question5.vue";
 
-import TC from '@/components/termsandcons.vue';
+import TC from "@/components/termsandcons.vue";
 
-import Messenger from '../components/Messenger.vue';
-import Identification from '../components/Identification.vue';
-
+import Messenger from "../components/Messenger.vue";
+import Identification from "../components/Identification.vue";
 
 @Component({
   components: {
