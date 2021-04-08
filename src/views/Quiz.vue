234536eceaede8903b4b441256b9758f7da84d8e
Michael Funk <michael.funk@ucalgary.ca>
2021-04-08 15:32:23 -0600

    Implemented Question 8 and 4 ready for linking

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -29,7 +29,9 @@
       <div v-if="this.view == 2">
         <Q3Card class="mx-auto m-3" />
       </div>
-      <div v-if="this.view == 3"></div>
+      <div v-if="this.view == 3">
+        <Q4Card class="mx-auto m-3" />
+        </div>
       <div v-if="this.view == 4">
         <Q5Card class="mx-auto m-3" />
       </div>
@@ -38,7 +40,8 @@
       <div v-if="this.view == 6">
         <Messenger class="mx-auto m-3"></Messenger>
       </div>
-      <div v-if="this.view == 7"></div>
+      <div v-if="this.view == 7"><Q8Card class="mx-auto m-3" /></div>
+        
       <div v-if="this.view == 8"></div>
       <div v-if="this.view == 9"></div>
     </div>
@@ -53,6 +56,7 @@ import Q1Card from "@/components/Question1Card.vue";
 import Q3Card from "@/components/Question3.vue";
 import Q4Card from "@/components/Question4.vue";
 import Q5Card from "@/components/Question5.vue";
+import Q8Card from "@/components/Question8.vue";
 
 import TC from "@/components/termsandcons.vue";
 
@@ -64,6 +68,7 @@ import Identification from "../components/Identification.vue";
     Q1Card,
     Q3Card,
     Q5Card,
+    Q8Card,
 
     TC,
 
