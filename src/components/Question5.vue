e2fb52e2b300d9fdec0b300886f9f63bddf21e67
LougheedWan <47652087+LougheedWan@users.noreply.github.com>
2021-04-08 12:16:11 -0600

    finished components

--- src/components/Question5.vue
+++ src/components/Question5.vue
@@ -10,12 +10,32 @@
 
         <h1 class="text-l font-bold pb-6">01001000 01100101 01101100 01101100 01101111 00100001 00100000 01010000 01101100 01100101 01100001 01110011 01100101 00100000 01100100 01101111 00100000 01101110 01101111 01110100 00100000 01110100 01101111 01110101 01100011 01101000 00100000 01110100 01101000 01101001 01110011 00100000 01100010 01110101 01110100 01110100 01101111 01101110 00101110</h1>
      
-        <button class = " max-w-xs bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto ">Click me!</button>
+        <button class = " max-w-xs bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto " @click="incrementcounter">Click me!</button>
 
         <h1 class="text-xs font-bold p-4">hint: remember! No one said this was a closed book test!</h1>
 
         <h1 class="text-2xl font-extrabold pb-6">When you think you got it...click continue below.</h1>
 
-        <button class = " max-w-xs bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto ">Continue</button>
+        <button type = "button" class = " max-w-xs bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto " >Continue</button>
      </div>
 </template>
+
+<script>
+
+export default {
+    
+    data() {
+        return{
+            counter:0
+        }
+        
+    },
+
+    methods:{
+        incrementcounter(){
+            this.counter++;
+            console.log(this.counter);
+        }
+    }
+}
+</script>
--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,9 +1,10 @@
 
 <template>
     <div>
-
-      <!-- <Q1Card/> -->
+      <TC/>
+      <Q1Card/>
        <Q5Card/>
+
     </div>
 </template>
 
@@ -13,12 +14,14 @@
 import {Component, Vue} from 'vue-property-decorator';
 import Q1Card from '@/components/Question1Card.vue';
 import Q5Card from '@/components/Question5.vue';
+import TC from '@/components/termsandcons.vue';
 
 
 @Component({
   components: {
     Q1Card,
     Q5Card,
+    TC,
   }
 })
 export default class Quiz1 extends Vue{
