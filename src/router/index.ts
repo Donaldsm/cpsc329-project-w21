c11ed2a1c2ae8bf9ee5ecabdebc2af380111540b
Donaldsm <Donald@theMcEacherns.com>
2021-04-07 22:51:09 -0600

    merged Donald

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
-import Quiz1 from '../views/Quiz.vue';
+import Quiz from '../views/Quiz.vue';
 
 Vue.use(VueRouter);
 
@@ -18,17 +18,10 @@ const routes: RouteConfig[] = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-<<<<<<< HEAD
-  },
-  {
-    path: '/quiz1',
-    name: 'Quiz1',
-=======
   },{
     path: '/quiz',
     name: 'Quiz',
->>>>>>> b1756424aae181d26861974e8d74c02c9277d32e
-    component: Quiz1,
+    component: Quiz,
   },
 ];
 
--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,10 +1,12 @@
 
 <template>
-    <div>
+    <div class="flex flex-col">
 
       <!-- <Q1Card/> -->
-       <Q5Card/>
-       <Q3Card/>
+       <Q5Card class="mx-auto m-3"/>
+       <Q3Card class="mx-auto m-3"/>
+       <Messenger class="mx-auto m-3"></Messenger>
+       <identification class="mx-auto m-3"></identification>
     </div>
 </template>
 
@@ -16,6 +18,8 @@ import Q1Card from '@/components/Question1Card.vue';
 import Q3Card from '@/components/Question3.vue';
 import Q4Card from '@/components/Question4.vue';
 import Q5Card from '@/components/Question5.vue';
+import Messenger from '../components/Messenger.vue';
+import Identification from '../components/Identification.vue';
 
 
 @Component({
@@ -24,6 +28,8 @@ import Q5Card from '@/components/Question5.vue';
     Q3Card,
     Q5Card,
     Q4Card,
+    Messenger,
+    Identification
   }
 })
 export default class Quiz1 extends Vue {
